8564ec7f8c904e5f83b6d673ed5eb72ccbbac660
ybetti <136556827+ybetti@users.noreply.github.com>
2024-10-21 11:17:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -279,7 +279,7 @@ function displaySurroundingValues(row, col) {
     }
 
     // 新しいウィンドウを開く
-    const newWindow = window.open('', '', 'width=400,height=400');
+    const newWindow = window.open('', '', 'width=400,height=300');
     
     // 5×5のテーブルを作成
     newWindow.document.write('<table border="1" style="border-collapse: collapse;">');
